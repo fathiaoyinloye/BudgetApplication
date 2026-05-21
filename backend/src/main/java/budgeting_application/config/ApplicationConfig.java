804a13687d273761c6fd8aa5973c81a3dac9b773
fathiaoyinloye <fathiaoyinloye21@gmail.com>
2026-05-21 04:12:09 +0100

    chore: handed dataintegrity exception

--- backend/src/main/java/budgeting_application/config/ApplicationConfig.java
+++ backend/src/main/java/budgeting_application/config/ApplicationConfig.java
@@ -25,7 +25,7 @@ public class ApplicationConfig {
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 
-
+//
     @Bean
     public AuthenticationProvider authenticationProvider(UserDetailsService userDetailsService) {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
